4e1e0de657c080d438fe2e0950ce5be5119d2294
Skye Book <skye.book@gmail.com>
2011-09-10 18:32:58 -0400

    Design page now generates the image link as well as the title, date, user, and description data

--- design.php
+++ design.php
@@ -17,128 +17,151 @@
 
 </head> 
 <body> 
-	<div class='master-container'> 
-		<header> 
-			<nav> 
-				<ul> 
-					<li class='logo'> 
-						<a href='/'> 
-							<img src='/images/logo-header.png'> 
-						</a> 
-					</li> 
-					<li> 
-						<a href='/webstart/betaville.jnlp'>Download</a> 
-					</li> 
-					<li> 
-						<a class='' href='/what-is-betaville'>Info</a> 
-					</li> 
-					<li> 
-						<a class='' href='/proposals'>Explore</a> 
-					</li> 
-					<li> 
-						<a class='' href='/contribute'>Contribute</a> 
-					</li> 
-				</ul> 
-				<div class='clear'></div> 
-			</nav> 
-		</header> 
-		<div class='page-container'> 
-			<div class='page-body container project' id='project'> 
-				<div class='project-container'> 
-					<h1>Shae's tribute to Philip Johnson</h1> 
-					<div class='project-meta'> 
-						<ul> 
-							<li> 
-								<strong> 
-									Author&nbsp;
-								</strong> 
-								susie
-								·
-							</li> 
-							<li> 
-								<strong> 
-									Last&nbsp;Update
-								</strong> 
-								2 months ago
-								·
-							</li> 
-							<li> 
-								<span class='comment'> 
-									<a href=''> 
-										<span class='count'> 
-											1
-										</span> 
-										comments,
-										<span class='count'> 
-											0
-										</span> 
-										likes
-									</a> 
+
+
+<?php
+// setup the basics
+
+include('config.php');
+$designID = $_GET['id'];
+
+// get the project information
+
+$designRequest = SERVICE_URL.'?section=design&request=findbyid&id='.$designID;
+$designJSON = file_get_contents($designRequest,0,null,null);
+$designOutput = json_decode($designJSON, true);
+$design = $designOutput['design'];
+
+$commentRequest = SERVICE_URL.'?section=comment&request=getforid&id='.$designID;
+$commentJSON = file_get_contents($commentRequest,0,null,null);
+$commentOutput = json_decode($commentJSON, true);
+$comments = $commentOutput['comments'];
+
+?>
+<div class='master-container'> 
+	<header> 
+		<nav> 
+			<ul> 
+				<li class='logo'> 
+					<a href='/'> 
+						<img src='/images/logo-header.png'> 
+					</a> 
+				</li> 
+				<li> 
+					<a href='/webstart/betaville.jnlp'>Download</a> 
+				</li> 
+				<li> 
+					<a class='' href='/what-is-betaville'>Info</a> 
+				</li> 
+				<li> 
+					<a class='' href='/proposals'>Explore</a> 
+				</li> 
+				<li> 
+					<a class='' href='/contribute'>Contribute</a> 
+				</li> 
+			</ul> 
+			<div class='clear'></div> 
+		</nav> 
+	</header> 
+	<div class='page-container'> 
+		<div class='page-body container project' id='project'> 
+			<div class='project-container'> 
+				<h1><?php echo $design['name']; ?></h1> 
+				<div class='project-meta'> 
+					<ul> 
+						<li> 
+							<strong> 
+								Author&nbsp;
+							</strong> 
+							<?php echo $design['user']; ?>
+							·
+						</li> 
+						<li> 
+							<strong> 
+								Last&nbsp;Update
+							</strong> 
+							<?php echo $design['date']; ?>
+							·
+						</li> 
+						<li> 
+							<span class='comment'> 
+								<a href=''> 
+									<span class='count'>
+										<?php
+									// count the number of comments
+									$commentCount=0;
+									foreach($comments as $comment){
+										$commentCount++;
+									}
+									echo $commentCount;
+									?>
 								</span> 
-							</li> 
-						</ul> 
-						<div class='button-row'> 
-							<a class='uberbutton green' href='/betaville.jnlp'> 
-								Launch
-								<span class='icon i-play'>9</span> 
+								comments,
+								<span class='count'> 
+									0
+								</span> 
+								likes
 							</a> 
-							<a class='uberbutton' href=''> 
-								Like
-								<span class='icon'>k</span> 
-							</a> 
-							<a class='uberbutton' href=''> 
-								Share
-								<span class='icon i-share'>G</span> 
-							</a> 
-						</div> 
-					</div> 
-					<div class='project-description'> 
-						Glass House 2.0
-					</div> 
-					<div class='discussion'> 
-						<h2> 
-							Discussion
-						</h2> 
-						<p> 
-							<em>To comment, find this project in Betaville.</em> 
-						</p> 
-						<ul> 
-
-
-
-							<?php
-						include('config.php');
-						$designID = 3894;
-						$commentRequest = SERVICE_URL.'?section=comment&request=getforid&id='.$designID;
-						$commentJSON = file_get_contents($commentRequest,0,null,null);
-						$commentOutput = json_decode($commentJSON, true);
-						$comments = $commentOutput['comments'];
-
-						foreach($comments as $comment){
-							?>
-							<li> 
-								<div class='discussion-meta column span-3'> 
-									<a class='author'><?php echo $comment['user']; ?></a> 
-									<div class='timestamp'>
-										<?php echo $comment['date'] ?>
-									</div> 
-								</div> 
-								<div class='discussion-body column span-9 last'>
-									<?php echo $comment['comment'] ?>
-								</div> 
-							</li> 
-							<?php
-					}
-					?>
-
-
+						</span> 
+					</li> 
 				</ul> 
+				<div class='button-row'> 
+					<a class='uberbutton green' href='/betaville.jnlp'> 
+						Launch
+						<span class='icon i-play'>9</span> 
+					</a> 
+					<a class='uberbutton' href=''> 
+						Like
+						<span class='icon'>k</span> 
+					</a> 
+					<a class='uberbutton' href=''> 
+						Share
+						<span class='icon i-share'>G</span> 
+					</a> 
+				</div> 
 			</div> 
-		</div> 
-		<aside> 
-			<img src='http://betaville.net/designthumbs/3894.png'> 
-		</aside> 
+			<div class='project-description'>
+				<?php echo $design['description']; ?>
+			</div> 
+			<div class='discussion'> 
+				<h2> 
+					Discussion
+				</h2> 
+				<p> 
+					<em>To comment, find this project in Betaville.</em> 
+				</p> 
+				<ul> 
+
+
+
+					<?php
+
+
+				foreach($comments as $comment){
+					?>
+					<li> 
+						<div class='discussion-meta column span-3'> 
+							<a class='author'><?php echo $comment['user']; ?></a> 
+							<div class='timestamp'>
+								<?php echo $comment['date'] ?>
+							</div> 
+						</div> 
+						<div class='discussion-body column span-9 last'>
+							<?php echo $comment['comment'] ?>
+						</div> 
+					</li> 
+					<?php
+			}
+			?>
+
+
+		</ul> 
 	</div> 
+</div> 
+<aside>
+	<?php echo "<img src='".THUMBNAIL_URL.$designID.".png'>" ?>
+</aside> 
+</div> 
 
 </div> 
 
