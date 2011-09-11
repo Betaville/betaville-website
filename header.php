5bd34a6eeb78344643c86c8215c1930fc70fcda7
Skye Book <skye.book@gmail.com>
2011-09-11 19:39:20 -0400

    Updated links in the header file

--- header.php
+++ header.php
@@ -2,14 +2,14 @@
 	<nav>
 		<ul>
 			<li class='logo'>
-				<a href='/'>
+				<a href='./'>
 					<img src='images/logo-header.png'>
 				</a>
 			</li>
-			<li><a href='/webstart/betaville.jnlp'>Download</a></li>
-			<li><a class='' href='/what-is-betaville'>Info</a></li>
-			<li><a class='' href='/proposals'>Explore</a></li>
-			<li><a class='' href='/contribute'>Contribute</a></li>
+			<li><a href='http://betaville.net/webstart/betaville.jnlp'>Download</a></li>
+			<li><a class='' href='what-is-betaville.php'>Info</a></li>
+			<li><a class='' href='proposals.php'>Explore</a></li>
+			<li><a class='' href='contribute.php'>Contribute</a></li>
 		</ul>
 		<div class='clear'></div>
 	</nav>
