3ad2ed6aaa37a9a41db4b0f46e793bde8d0a76d9
Skye Book <skye.book@gmail.com>
2011-09-11 19:36:52 -0400

    Added contribute page

---
+++ contribute.php
@@ -0,0 +1,75 @@
+
+<!doctype html> 
+<html> 
+<head> 
+	<title>Betaville</title> 
+	<link href='stylesheets/reset.css' rel='stylesheet'> 
+	<link href='stylesheets/screen.css' rel='stylesheet'> 
+	<script src='js/jquery-1.4.2.min.js'></script> 
+	<script src='js/jquery.jcarousel.min.js'></script> 
+	<script src='js/betaville.home.js'></script> 
+	<script src='js/jquery.easing.1.3.js'></script> 
+	<script src='fancybox/jquery.mousewheel-3.0.4.pack.js' type='text/javascript'></script> 
+	<script src='fancybox/jquery.fancybox-1.3.4.pack.js' type='text/javascript'></script> 
+	<link href='fancybox/jquery.fancybox-1.3.4.css' media='screen' rel='stylesheet' type='text/css'> 
+	<meta content='text/html;charset=utf-8' http-equiv='Content-Type'> 
+	<meta name="csrf-param" content="authenticity_token"/> 
+	<meta name="csrf-token" content="kg1Klytrjq1CyeFy3G1cujAERmXA69mxelZXrv9FcFc="/> 
+
+</head> 
+<body> 
+	<div class='master-container'> 
+		<?php include('header.php'); ?>
+		<div class='decoration'> 
+			<div class='page-title'> 
+				<h1>Contribute to Betaville</h1> 
+			</div> 
+		</div> 
+		<div class='page-container'> 
+			<div class='page-body container' id='contribute'> 
+				<div class='column contribute-comment span-7 append-1'> 
+					<h1>Comment</h1> 
+					<a href='http://betaville.net/webstart/betaville.jnlp'> 
+						<div class='shot'></div> 
+					</a> 
+					<p> 
+						Explore the Betaville world and express your opinion about the proposals that have been uploaded. By engaging in a conversation with the creator of the proposal, you can influence its future development.
+					</p> 
+					<p> 
+						<a href='http://betaville.net/webstart/betaville.jnlp'>Download Betaville »</a> 
+						to see proposals and comment on them.
+					</p> 
+				</div> 
+				<div class='column contribute-design span-7 append-1'> 
+					<h1>Design</h1> 
+					<a href='http://wiki.betaville.net/doku.php?id=maya'> 
+						<div class='shot'></div> 
+					</a> 
+					<p> 
+						By using simple modeling tools such as Google SketchUp, you can bring into life your own ideas and import them to Betaville for others to see.
+					</p> 
+					<p> 
+						<a href='http://wiki.betaville.net/doku.php?id=maya'>Visit our wiki »</a> 
+						for tutorials about modeling and uploading proposals to Betaville.
+					</p> 
+				</div> 
+				<div class='column contribute-develop span-7 last'> 
+					<h1>Develop</h1> 
+					<a href='http://wiki.betaville.net/doku.php?id=howto:check_out_and_run'> 
+						<div class='shot'></div> 
+					</a> 
+					<p> 
+						Betaville Project is open–source and contributes to some libraries such as JMoneyEngine and FengGUI.
+					</p> 
+					<p> 
+						<a href='http://wiki.betaville.net/doku.php?id=howto:check_out_and_run'>Check out our wiki »</a> 
+						on how to contribute code.
+					</p> 
+				</div> 
+			</div> 
+		</div> 
+
+		<?php include('footer.php'); ?>
+	</div> 
+</body> 
+</html> 
