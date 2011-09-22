8bcf6eadd9a9ffbd7820a834b9414ec9c06add88
Skye Book <skye.book@gmail.com>
2011-09-22 16:12:04 -0400

    Added head file to replace frequently repeated content

---
+++ head.php
@@ -0,0 +1,13 @@
+<title>Betaville</title> 
+	<link href='stylesheets/reset.css' rel='stylesheet'> 
+	<link href='stylesheets/screen.css' rel='stylesheet'> 
+	<script src='js/jquery-1.4.2.min.js'></script> 
+	<script src='js/jquery.jcarousel.min.js'></script> 
+	<script src='js/betaville.home.js'></script> 
+	<script src='js/jquery.easing.1.3.js'></script> 
+	<script src='fancybox/jquery.mousewheel-3.0.4.pack.js' type='text/javascript'></script> 
+	<script src='fancybox/jquery.fancybox-1.3.4.pack.js' type='text/javascript'></script> 
+	<link href='fancybox/jquery.fancybox-1.3.4.css' media='screen' rel='stylesheet' type='text/css'> 
+	<meta content='text/html;charset=utf-8' http-equiv='Content-Type'> 
+	<meta name="csrf-param" content="authenticity_token"/> 
+	<meta name="csrf-token" content="kg1Klytrjq1CyeFy3G1cujAERmXA69mxelZXrv9FcFc="/> 
