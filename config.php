d86302de97ce028c7bfc70bac9f9701e9220fd77
Skye Book <skye.book@gmail.com>
2011-10-11 17:35:02 -0400

    Fixed formatting in index.php; restored config.php to using localhost

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 define('WEB_URL', 'http://localhost/betaville-website');
-define('SERVICE_URL', 'http://robert.betaville.net/service/service.php');
-define('THUMBNAIL_URL', 'http://robert.betaville.net/storage/designthumbs/');
+define('SERVICE_URL', 'http://localhost/service/service.php');
+define('THUMBNAIL_URL', 'http://localhost/storage/designthumbs/');
 ?>
