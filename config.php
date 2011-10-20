8b1c849111d393a8553912716b61b3359d39457f
Megh Vora <meghrvora@gmail.com>
2011-10-20 16:08:00 -0400

    Updated config file

--- config.php
+++ config.php
@@ -3,4 +3,3 @@ define('WEB_URL', 'http://localhost/betaville-website');
 define('SERVICE_URL', 'http://robert.betaville.net/service/service.php');
 define('THUMBNAIL_URL', 'http://robert.betaville.net/storage/designthumbs/');
 ?>
-
