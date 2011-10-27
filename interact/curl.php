a56bd25375e1f0c143384da4ddce2f1f10c4942c
Skye Book <skye.book@gmail.com>
2011-10-26 20:42:44 -0400

    updated the curl script

--- interact/curl.php
+++ interact/curl.php
@@ -1,14 +1,23 @@
 <?php
-function download($url){
-	if(!isset($_SESSION['cookiefile'])){
-		echo "new";
-		$_SESSION['cookiefile'] = tempnam ("/tmp", "CURLCOOKIE");
-	}
-	else echo "old";
 
-	$ch = curl_init ($url);
-	curl_setopt ($ch, CURLOPT_COOKIEFILE, $_SESSION['cookiefile']); 
-	curl_setopt ($ch, CURLOPT_RETURNTRANSFER, true);
-	return curl_exec($ch);
+// Check if a curl handle is already setup for this session
+if(!isset($_SESSION['curl'])){
+	// if it curl hasn't been initialized, set it up
+	$curl = curl_init();
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		if(strpos($_SERVER['SERVER_SOFTWARE'], "Win")===false){
+			curl_setopt($curl, curlopt_cookiejar, "/dev/null");
+		}
+		else{
+			curl_setopt($curl, curlopt_cookiejar, "NUL");
+		}
+		
+	// Assign curl to the session variable
+	$_SESSION['curl'] = $curl;
+}
+
+function bv_download($url){
+	curl_setopt($_SESSION['curl'], CURLOPT_URL, $url);
+	return curl_exec($_SESSION['curl']);
 }
 ?>
--- login.php
+++ login.php
@@ -18,9 +18,15 @@ $userActivated  = json_decode($temp1, true);
 if (!$userActivated){
 	echo "Please activate your account before signing in <br />";
 	exit();
-} 
+}
+
+include("interact/curl.php");
+
+// setup CURL cookies when first logging in
 $loginAuth = SERVICE_URL.'?section=user&request=auth&username='.$_POST['user'].'&password='.$_POST['pass'];
-$temp = file_get_contents($loginAuth,0,null,null);
+$temp = download($loginAuth);
+
+
 $loginAuthOutput = json_decode($temp, true);
 if ( !($loginAuthOutput['authenticationSuccess'])  ){
 	echo "Username or password is invalid, Please try again <br />";
@@ -33,9 +39,12 @@ else {
 	$_SESSION['token'] = $loginAuthOutput['token'];
 	$_SESSION['logged'] = true;
 	
+	// this next line is for debugging only and should be removed
+	$_SESSION['size'] = $loginAuthOutput['size'];
+	
 	// grab the user's type
 	$userRequest = SERVICE_URL.'?section=user&request=getlevel&username='.$_SESSION['username'];
-	$userJSON = file_get_contents($userRequest,0,null,null);
+	$userJSON = download($userRequest);
 	$userOutput = json_decode($userJSON, true);
 	$_SESSION['userType'] = $userOutput['userType'];
 }
