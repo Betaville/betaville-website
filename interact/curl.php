a1d2b2771374be8ab7c67b0af2973347409f4de1
Skye Book <skye.book@gmail.com>
2011-10-26 23:02:10 -0400

    curl functionality updated to attempt using a temp file

--- interact/curl.php
+++ interact/curl.php
@@ -2,6 +2,8 @@
 
 // Check if a curl handle is already setup for this session
 if(!isset($_SESSION['curl'])){
+	
+	/*
 	// if it curl hasn't been initialized, set it up
 	$curl = curl_init();
 	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
@@ -14,10 +16,24 @@ if(!isset($_SESSION['curl'])){
 		
 	// Assign curl to the session variable
 	$_SESSION['curl'] = $curl;
+	*/
+}
+
+if(!isset($_SESSION['curlcookie'])){
+	
+	echo "creating";
+	
+	$ckfile = tempnam ("/tmp", "CURLCOOKIE");
+	$_SESSION['curlcookie'] = $ckfile;
 }
 
 function bv_download($url){
-	curl_setopt($_SESSION['curl'], CURLOPT_URL, $url);
-	return curl_exec($_SESSION['curl']);
+	//curl_setopt($_SESSION['curl'], CURLOPT_URL, $url);
+	//return curl_exec($_SESSION['curl']);
+	
+	$ch = curl_init ($url);
+	curl_setopt ($ch, CURLOPT_COOKIEFILE, $_SESSION['curlcookie']); 
+	curl_setopt ($ch, CURLOPT_RETURNTRANSFER, true);
+	return curl_exec ($ch);
 }
 ?>
