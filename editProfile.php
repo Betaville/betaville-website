6cbb1b7d86eaada60f9251bc1ebfc989375be8a9
Skye Book <skye.book@gmail.com>
2011-11-08 13:56:00 -0500

    commit fix

--- editProfile.php
+++ editProfile.php
@@ -8,9 +8,9 @@
 		//include('config.php');
 		
 		// get user information
-	//	$userRequest = SERVICE_URL.'?section=user&request=getpublicinfo&username='.$userName;
+		$userRequest = SERVICE_URL.'?section=user&request=getpublicinfo&username='.$userName;
 	//	$userJSON = file_get_contents($userRequest,0,null,null);
-		$userJSON = file_get_contents("userScandgolden24.txt");
+		//$userJSON = file_get_contents("userScandgolden24.txt");
 		$userOutput = json_decode($userJSON, true);
 		$user = $userOutput['userInfo'];
 		
