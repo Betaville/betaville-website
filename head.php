19a86b9a391b3ec8ce44ce9528e24f1c3f5e5ed5
Skye Book <skye.book@gmail.com>
2011-11-21 16:33:51 -0500

    removed the bundled jQuery library and instead linked it to the Google APIs CDN

--- head.php
+++ head.php
@@ -8,8 +8,8 @@ ob_start();
 <title>Betaville</title> 
 	<link href='./stylesheets/reset.css' rel='stylesheet'> 
 	<link href='./stylesheets/screen.css' rel='stylesheet'> 
-	<script src='./js/jquery-1.4.2.min.js'></script> 
-	<script src='./js/jquery.jcarousel.min.js'></script> 
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js"></script>
+	<script src='./js/jquery.jcarousel.min.js'></script>
 	<script src='./js/betaville.home.js'></script> 
 	<script src='./js/jquery.easing.1.3.js'></script> 
 	<script src='./fancybox/jquery.mousewheel-3.0.4.pack.js' type='text/javascript'></script> 
