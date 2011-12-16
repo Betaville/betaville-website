db09467201f5f751711f4e4426cb04b0caf78a03
Ibrahim Jumkhawala <ijumkh01@students.poly.edu>
2011-12-16 12:28:49 -0500

    Fix on bug where profile.php not accesible when user not logged in

--- profile.php
+++ profile.php
@@ -2,16 +2,7 @@
 include('head.php');
 include('config.php');
 ?>
-	<?php
-		if (!isset($_SESSION['logged']) && !$_SESSION['logged'] == true ) {
-	?>
-	<script type="text/javascript" >
-		window.location=<?php echo '"'.$WEB_URL.'"';?>;
-	</script>
-	<?php
-		}
-	?>
-    <style>
+<style>
 	.userInfo{
 		background-color:
 	}
