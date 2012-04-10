234304c5fbf139c88962399af26c9a6583d0f855
ibby <ibby.j@hotmail.com>
2012-04-10 14:55:21 -0400

    Readable names, indenting, copywright info....

--- edit-proposal-group.php
+++ edit-proposal-group.php
@@ -1,4 +1,21 @@
 <?php 
+/**  
+ *  edit-proposal - Change permission functionality on proposals, editable by the proposal owner, allows adding user to a group, deleting from group, changing permissions for the particular proposal......
+ *  Copyright (C) 2011-2012 Betaville
+ *  
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 	include('head.php');
 	include_once('betaville-functions.php');
 	include_once('config.php');
@@ -10,10 +27,11 @@
 	if (!isset($_SESSION['logged']) && !$_SESSION['logged'] == true ) {
 			header("Location: ".WEB_URL);
 	}
-	
+	//If the viewability parameter is changed
 	if(isset($_GET['view'])) {
+		//Get the value from the button
 		if(isset($_POST['button'])) {
-			echo "Proposal permission changed";
+			echo '<strong>Proposal permission changed</strong>';
 			$designID = $_GET['id'];
 			$permission = $_POST['button'];
 			$addPermission = SERVICE_URL.'?section=proposal&request=setpermission&id='.$designID.'&token='.$_SESSION['token'].'&permission='.$permission;
@@ -21,15 +39,14 @@
 			$addPermissionOutput = json_decode($addPermissionJSON, true);
 		}
 	}
-	
+	//Delete the user from the group
 	if(isset($_GET['deleteuser'])) {
 		$deletename=$_GET['deleteuser'];
 		$designid=$_GET['id'];
 		DeleteProposalGroupUser($designid,$deletename);
-		
 	    header('url='.WEB_URL.'/edit-proposal-group.php?id='.$designid); 
-		}
-
+	}
+	//Add a user to the group, few checks if the user already exists or if the user owns the design
 	if(isset($_GET['adduser'])) {
 		if(checkUserInProposalGroup($_GET['adduser'],$_GET['id']) != true && $_SESSION['username'] != $_GET['adduser']) {
 			$userToAdd=$_GET['adduser'];
@@ -38,7 +55,6 @@
 			$addRequest = SERVICE_URL.'?section=user&request=addusertogroup&addname='.$userToAdd.'&designid='.$designid;
 			$addJSON = file_get_contents($addRequest,0,null,null);
 			$addOutput = json_decode($addJSON, true);
-			
 		    header('url='.WEB_URL.'/edit-proposal-group.php?id='.$designid); 
 		}
 		else {
@@ -48,34 +64,27 @@
 			else {
 			$designid = $_GET['id'];
 			echo '<strong>'.$_GET['adduser'].' exists in group.</strong>';
-		       header('Refresh: 2;url='.WEB_URL.'/edit-proposal-group.php?id='.$designid); 	
+	        header('Refresh: 2;url='.WEB_URL.'/edit-proposal-group.php?id='.$designid); 	
 			}
 		}
 	}
-
+		//Get the design information.
 		$designid = $_GET['id'];
 		$designRequest = SERVICE_URL.'?section=design&request=findbyid&id='.$designid;
 		$designJSON = file_get_contents($designRequest,0,null,null);
 		$designOutput = json_decode($designJSON, true);
 		$design = $designOutput['design'];
-		
 			if($_SESSION['username']==$design['user']) {
-	
 		?>
         
         <div class="page">
         <div class="user">
-	
-        	<a class="fancyButton" href="design.php?id=<?php echo $designid;?>"><span>Back to Proposal</span></a><br>
-
-		
-		<br>
-
-        	
+			<a class="fancyButton" href="design.php?id=<?php echo $designid;?>"><span>Back to Proposal</span></a><br><br>
+    
         <h1><?php echo $design['name']; ?> </h1>
         	
             <h3>Edit Viewability</h3>		
-		<!-- The owner of a proposal can choose to show proposal to all, or show proposal to only him and group, if proposal does not need to be shown to a group user then delete him from group-->
+			<!-- The owner of a proposal can choose to show proposal to all, or show proposal to only him and group, if proposal does not need to be shown to a group user then delete him from group-->
             <form name="userInput" action ="edit-proposal-group.php?id=<?php echo $designid;?>&view=changed" method="post">
                 <input type="radio" name="button" value="all" /> Open to all<br>
                 <input type="radio" name="button" value="user_group" /> Me and Group<br>
@@ -84,7 +93,7 @@
             <br>
          	<h3>  Add User to Group</h3>
 			
-			<h4> Search for Users </h4>
+			<h4> Search users to add</h4>
 	
 			<script type="text/javascript">
 						function showResult(str){
@@ -130,13 +139,12 @@
 						</form>
 						<?php
 								if($_GET['action']=='edituser'){
-									$myuser = $_GET['target'];
-									echo '<b> Add </bold>'.$myuser.' ?</b>';
-									$url = WEB_URL."/edit-proposal-group.php?&adduser=".$myuser."&id=".$design['designID'];
+									$user_to_search = $_GET['target'];
+									echo '<b> Add </bold>'.$user_to_search.' ?</b>';
+									$url = WEB_URL."/edit-proposal-group.php?&adduser=".$user_to_search."&id=".$design['designID'];
 									echo '<nr><form name="userInput" action ="'.$url.'" method="post">
 									<input type="submit" name="submit" value="Add" onClick="return confirmPost()" /></form>';}
 								}
-
 								echo '<br>';
 						?>
 			<h3> Delete User from Group</h3>
@@ -147,8 +155,8 @@
 							$userOutput = json_decode($userJSON, true);
 							$users = $userOutput['users'];
 							echo '<h4> Current Users in Group:  </h4>';
-							foreach($users as $musy) {
-								echo '<a href="edit-proposal-group.php?deleteuser='.$musy.'&id='.$design['designID'].'" <strong>'.$musy.'<br></strong></a>';
+							foreach($users as $user_name) {
+								echo '<a href="edit-proposal-group.php?deleteuser='.$user_name.'&id='.$design['designID'].'" <strong>'.$user_name.'<br></strong></a>';
 							}?>
 		</div>
 
