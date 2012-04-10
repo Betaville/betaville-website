1c6d89350555ad7f2550b25c3788e70849bdbf7a
ibby <ibby.j@hotmail.com>
2012-04-10 14:36:40 -0400

    copywright...

--- config.php
+++ config.php
@@ -1,8 +1,24 @@
 <?php
+/**  
+ *  Config File- Basic configuration to for images, web url and the service url for connectivity
+ *  Copyright (C) 2011-2012 Betaville
+ *  
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // Set the default timezone
 date_default_timezone_set('America/New_York');
 
 define('WEB_URL', 'http://localhost/betaville-website');
-define('SERVICE_URL', 'http://localhost/service/service.php');
-define('THUMBNAIL_URL', 'http://localhost/storage/designthumbs/');
+define('SERVICE_URL', 'http://localhost/Betaville-Web-Service/service.php');
+define('THUMBNAIL_URL', 'http://robert.betaville.net/storage/designthumbs/');
 ?>
