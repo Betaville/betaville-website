354d9062504268a0569234933aed5094d38e7ad5
ibby <ibby.j@hotmail.com>
2012-04-10 13:20:24 -0400

    Copywright information

--- contribute.php
+++ contribute.php
@@ -1,4 +1,21 @@
-<?php include('head.php'); ?> 
+<?php include('head.php'); 
+/**  
+ *  Contribute page - Links to the wiki, Application download and information for developers to contribute!!
+ *  Copyright (C) 2011-2012 Betaville
+ *  
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */?> 
 <body> 
 	<div class='master-container'> 
 		<div class='decoration'> 
