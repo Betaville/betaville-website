94d16f1af1d7d01fd337cda78a086855fa8d8c3b
ibby <ibby.j@hotmail.com>
2012-04-10 13:11:30 -0400

    Indentation, comments on forgot password file

--- ForgotPassword.php
+++ ForgotPassword.php
@@ -1,23 +1,38 @@
 <?php 
+/**  
+ *  ForgotPassWord File - A service file to implement forgot password functionality for the website
+ *  Copyright (C) 2011-2012 Betaville
+ *  
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 ob_start(); 
 include('head.php');
-
-
+	//Check if the email has been sent, if yes, change code on that column for the user and send the verification email, otherwise send a message that the user has not been activated.
 	if(isset($_GET['sentMail'])) {
-		
 		$passChangeRequest = SERVICE_URL.'?section=user&request=changeCode&email='.urlencode($_POST['email']).'&websiteUrl='.WEB_URL.'/ForgotPassword.php?newCode=';
 		$changeJSON = file_get_contents($passChangeRequest,0,null,null);
 		$changeOutput = json_decode($changeJSON, true);
 		$newCode = $changeOutput['codeChangeSuccess'];
-		if($newCode===true) {
-			echo "verification email has been sent, click on the link there";
-		}
-		else {
-			echo "You have not been activated yet, click on the email link to get activated";
-		}		
+			if($newCode===true) {
+				echo "verification email has been sent, click on the link there";
+			}
+			else {
+				echo "You have not been activated yet, click on the email link to get activated";
+			}		
 		//header('Location:'.WEB_URL.'/ForgotPassword.php?newCode='.$a);
-		}
-	
+	}
+	//The same page is further used for changing the password, that is the link from the email with the new code, check for the code in the database, if it is correct then give a newpassword change request, else throw a failure message.
 	else if(isset($_GET['newCode'])) {
 		if(isset($_GET['newCode'])) {
 			$code=$_GET['newCode'];
@@ -25,33 +40,26 @@ include('head.php');
 			$changeJSON = file_get_contents($passChangeRequest,0,null,null);
 			$changeOutput = json_decode($changeJSON, true);
 			$newCode = $changeOutput['codeChangeSuccess'];
-				if($newCode===true) {?>
-				
-
-
+				if($newCode===true) {?>		
 					<script type="text/javascript">
-			
 						function validateForm() {
 							var x=document.getElementById("newPass1");
 							var y=document.getElementById("newPass2");
-							if (x.value=="" || y.value==""){
-		              					  alert("Insert Both the Values");
-								x.focus();
-								  return false;
-							  }		
+								if (x.value=="" || y.value==""){
+									alert("Insert Both the Values");
+									x.focus();
+									return false;
+								}		
 							if(x.value!=y.value){
 								alert("Both the passwords should be same");
 								x.value=null;
 								y.value=null;
 								x.focus();
 								return false;
-							  }
+						    }
 							return true;
 						}	
-					</script>
-	
-
-
+				    </script>
 	<br><br>
 	<center><h2>Update Password</h2></center>
 				<center><div id="myDiv" style="display:none"><h2></h2></div></center>
@@ -62,26 +70,24 @@ include('head.php');
 				<center><input  type="submit" id="submit" value="Change pass" onClick="return validateForm()"></center>
 				</form> 
 <?
-				}
-				else {
-					echo '<strong>'."Validation failed".'</strong>';
-				}	
+			}
+			else {
+				echo '<strong>'."Validation failed".'</strong>';
+			}	
 		}
 	}	
+	//Check for a proper email format and send the message
 	elseif(isset($_GET['sendVeri'])) {
 	?>
-	
-	<script type="text/javascript">
-	
+	<script type="text/javascript">	
 		function validateEmail(){
-
 			var x=document.forms["credentials"]["email"].value;
 			var atpos=x.indexOf("@");
 			var dotpos=x.lastIndexOf(".");
 				if (atpos<1 || dotpos<atpos+2 || dotpos+2>=x.length) {
 					alert("Not a valid e-mail address");
   					return false;
-				  }
+				}
 		}
 	</script>
 					<center><h2>Enter Your Email </h2></center>
@@ -93,6 +99,7 @@ include('head.php');
 				</form> 
 	<?	
 	}
+	//Check new code is same and change the password otherwise throw a validation failure.
 	else if(isset($_GET['changed'])&&isset($_GET['code'])) {
 		$code=$_GET['code'];
 		$password=$_POST['newPass1'];
@@ -108,10 +115,6 @@ include('head.php');
 		}
 		
 	}
-
-	
-
-	
 	else {}
 	include('footer.php'); 
 	?>
