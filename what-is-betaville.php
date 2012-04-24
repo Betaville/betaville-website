abd59a61f7621c1015ee5ddcceda36b2e1752254
ibby <ibby.j@hotmail.com>
2012-04-24 16:28:37 -0400

    cp info changed

--- what-is-betaville.php
+++ what-is-betaville.php
@@ -1,4 +1,22 @@
-<?php include('head.php'); ?>
+<?php 
+/**  
+ *  Page containing betaville information about betaville!
+ *  Copyright (C) 2011-2012 Betaville
+ *  
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+ include('head.php'); ?>
 <body> 
 	<div class='master-container'>
 		<div class='page-container' style='margin-top: 20px;'> 
